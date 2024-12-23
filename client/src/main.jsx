d22ccf611c97aa058484676cc0e87fb71bfecf2c
AbdelhakMHD <ae.mehda@gmail.com>
2024-12-23 12:20:46 +0100

    edit client env

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -16,7 +16,7 @@ const queryClient = new QueryClient({
   }
 })
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
-axios.defaults.baseURL = '';
+axios.defaults.baseURL = API_BASE_URL + '/api/v1';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <QueryClientProvider client={queryClient}>
